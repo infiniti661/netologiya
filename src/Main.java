d052a5b768d3210a3ed129eb4825d1a3e3bb6b8a
Dilmar <infiniti661@yandex.ru>
2025-05-20 13:31:12 +0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int income = 44000;
-        int spending = 30000;
-        System.out.println("Итого (руб):");
-        System.out.println(income - spending);
+        System.out.println("Artem Mikhaylov");
     }
 }
